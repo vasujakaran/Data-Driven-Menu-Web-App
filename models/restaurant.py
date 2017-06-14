1c236e548adc07c574d8054060bd96f97db97b5e
ramu kaka <rkaka@jungle.net>
2017-06-14 00:45:19 -0400

    Added some comments on Restaurant model.
    Can run pydoc to see it's documentation
    
    like this on the command prompt
    $pydoc models.restaurant

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,14 +1,35 @@
+'''@package models
+'''
+
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from . import database_setup
 
 
+
 # Class
 class Restaurant(database_setup.Base):
+    '''
+    The description of a restaurant.
+    A restaurant is an entity that consists of n menuitems.
+
+    Ideally, there should be another model called outlet.
+    A restaurant should have many outlets.
+    And outlets should have menu items.
+
+    Must also have a relation (FK) to another table called cusines.
+    '''
     # Table
     __tablename__ = 'restaurant'
+
     # Mapper
+    'Primary Key'
     id = Column(Integer, primary_key=True)
+
+    'Name of the establishment'
     name = Column(String(80), nullable=False)
+
+    'Address of the establishment'
     address = Column(String(250), nullable=False)
 
     # this property is from Restaurant
